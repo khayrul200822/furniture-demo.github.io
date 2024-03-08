227af326c8556a31efe81e7860a91c17e6529190
khayrul200822 <khayrulislam200822@gmail.com>
2024-03-08 23:15:01 +0600

    updated banner

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+
+//offer banner handler
+const banner_close_button = document.querySelector(".banner_close");
+const banner_area = document.querySelector(".offer_banner");
+
+banner_close_button.addEventListener("click", () => {
+    banner_area.style.transform = "translate(0%, -100%)"
+    banner_area.style.visibility = "hidden"
+    banner_area.style.opacity = "0"
+    banner_area.style.animation = "hide .2s ease-in .2s alternate forwards"
+})
